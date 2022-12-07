4fcc55b945bb88b28855dc65c2c84007f58c2dd1
Kristel Fung <kristelfung@berkeley.edu>
2022-12-07 13:01:04 -0800

    eating carrots success

--- World/Carrots.gd
+++ World/Carrots.gd
@@ -1,6 +1,4 @@
 extends Node2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("eat"):
-		queue_free()
+func _on_Hurtbox_area_entered(area):
+	queue_free()
